fee14e730d9c095f7fdbbe7ee18f4f0be9100718
woodspring-toronto <32200982+woodspring-toronto@users.noreply.github.com>
2018-02-12 23:27:56 -0500

    Add files via upload

---
+++ Spring4Client/src/main/java/woodspring/spring4client/Spring4ClientApplication.java
@@ -0,0 +1,12 @@
+package woodspring.spring4client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4ClientApplication.class, args);
+	}
+}
