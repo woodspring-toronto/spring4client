d2ec2819d7a3718619e76d25d21e4fd48d844f3b
woodspring-toronto <32200982+woodspring-toronto@users.noreply.github.com>
2018-02-12 23:29:29 -0500

    Spring4Contact

---
+++ Spring4Contact/src/main/java/woodspring/spring4contact/Spring4ContactApplication.java
@@ -0,0 +1,12 @@
+package woodspring.spring4contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4ContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4ContactApplication.class, args);
+	}
+}
---
+++ Spring4Contact/src/test/java/woodspring/spring4contact/Spring4ContactApplicationTests.java
@@ -0,0 +1,16 @@
+package woodspring.spring4contact;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring4ContactApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
