ba16b0fd5791f6c36b410b39ead2293740f3e0ba
woodspring-toronto <32200982+woodspring-toronto@users.noreply.github.com>
2018-02-12 23:30:09 -0500

    Spring4Employee

---
+++ Spring4Employee/src/main/java/woodspring/spring4employee/Spring4EmployeeApplication.java
@@ -0,0 +1,12 @@
+package woodspring.spring4employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4EmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4EmployeeApplication.class, args);
+	}
+}
